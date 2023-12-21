29f5153675806dbd03cdd9a82c1d0921463df8e1
gadname <gakugaku1114@icloud.com>
2023-12-21 15:51:11 +0900

    carrierwave gemの追加

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_19_112152) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_21_013539) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_19_112152) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "board_image"
   end
 
   create_table "users", force: :cascade do |t|

6a0596571745b5cecfbc914561a5860eec5384e4
gadname <gakugaku1114@icloud.com>
2024-01-07 15:26:35 +0900

    step入力の実装

--- front/src/app/components/input-text.tsx
+++ front/src/app/components/input-text.tsx
@@ -4,18 +4,18 @@ import { inputTextTypes } from './types'
 // テキストフォーム
 const InputText = ({ handleKeyPress, inputRef, loading }: inputTextTypes) => {
   return (
-    <group position={[0, 0.3, 0.05]}>
+    <group position={[0, 0.4, 0.05]}>
       <Html center>
         {loading ? (
           <div className="flex justify-center">
-            <div className="h-7 w-7 animate-spin rounded-full border-2 border-white border-t-transparent" />
+            <div className="h-8 w-7 animate-spin rounded-full border-2 border-white border-t-transparent" />
           </div>
         ) : (
           <div>
             <input
               type="text"
               ref={inputRef}
-              className="text-black rounded px-2 py-1 outline-none w-[270px] bg-white bg-opacity-40 shadow"
+              className="text-black rounded px-2 py-2 outline-none w-[200px] bg-white bg-opacity-40 shadow"
               onKeyDown={handleKeyPress}
             />
           </div>

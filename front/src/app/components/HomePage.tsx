9c3c49f7c0bd4970e2142f65406069d8a210c452
gadname <gakugaku1114@icloud.com>
2024-01-23 23:58:33 +0900

    AIs.jpgの変更

--- front/src/app/components/HomePage.tsx
+++ front/src/app/components/HomePage.tsx
@@ -10,7 +10,7 @@ const images = [
   { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: '/art2.png'  },
   // Left
   { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0],url: '/art3.png'  },
-  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0],url: '/Ais.jpg' },
+  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0],url: '/AIs.jpg' },
 //   { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: '/ai8.jpg' },
 //   // Right
   { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0],  url: '/ai6.jpg'},

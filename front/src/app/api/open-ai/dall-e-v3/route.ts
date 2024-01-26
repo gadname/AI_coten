e9ba277cda2e27dfcaaec7ff623a613a0220afeb
gadname <gakugaku1114@icloud.com>
2024-01-26 19:08:53 +0900

    dall-e-3修正

--- front/src/app/api/open-ai/dall-e-v3/route.ts
+++ front/src/app/api/open-ai/dall-e-v3/route.ts
@@ -1,4 +1,4 @@
-import { generateImageWithDallE3 } from "@/services/open-ai";
+import { askGptV3_5Turbo, generateImageWithDallE3 } from "@/services/open-ai";
 
 export async function POST(req: Request) {
   const { textPrompt } = await req.json();
@@ -10,13 +10,6 @@ export async function POST(req: Request) {
     );
   }
 
-  try {
-    const srcUrl = await generateImageWithDallE3(textPrompt);
-    return new Response(JSON.stringify({ srcUrl }), { status: 200 });
-  } catch (error) {
-    if (error.status !== 200) {
-      throw new Error(`Request failed with status ${error.status}`);
-    }
-    // ... その他のエラーハンドリング ...
-  }
+  const srcUrl = await generateImageWithDallE3(textPrompt);
+  return new Response(JSON.stringify({ srcUrl }), { status: 200 });
 }

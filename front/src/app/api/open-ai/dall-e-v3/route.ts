63820dbde4178508ce4718c81f5f26836a64be02
gadname <gakugaku1114@icloud.com>
2024-01-26 16:04:07 +0900

    API修正3

--- front/src/app/api/open-ai/dall-e-v3/route.ts
+++ front/src/app/api/open-ai/dall-e-v3/route.ts
@@ -1,8 +1,6 @@
 import { askGptV3_5Turbo, generateImageWithDallE3 } from "@/services/open-ai";
 import type { NextApiRequest, NextApiResponse } from 'next';
-export const config = {
-  runtime: 'experimental-edge',
-};
+export const runtime = 'experimental-edge';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {

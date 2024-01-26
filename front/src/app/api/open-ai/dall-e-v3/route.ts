1cd3b867946e184b8d437334a9e1cf4479c2adb9
gadname <gakugaku1114@icloud.com>
2024-01-26 15:56:26 +0900

    OpenAPIの修正2

--- front/src/app/api/open-ai/dall-e-v3/route.ts
+++ front/src/app/api/open-ai/dall-e-v3/route.ts
@@ -1,24 +1,19 @@
 import { askGptV3_5Turbo, generateImageWithDallE3 } from "@/services/open-ai";
-
+import type { NextApiRequest, NextApiResponse } from 'next';
 export const config = {
   runtime: 'experimental-edge',
 };
 
-export default async function handler(req) {
-  // Edge Functionsはリクエストを受け取るときにRequestオブジェクトを使用します
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
-    return new Response('Method Not Allowed', { status: 405 });
+    return res.status(405).send('Method Not Allowed');
   }
 
-  const { textPrompt } = await req.json();
-
+  const { textPrompt } = req.body;
   if (typeof textPrompt !== 'string') {
-    return new Response(
-      JSON.stringify({ error: 'textPrompt must be a string' }),
-      { status: 400, headers: { 'Content-Type': 'application/json' } }
-    );
+    return res.status(400).json({ error: 'textPrompt must be a string' });
   }
 
   const srcUrl = await generateImageWithDallE3(textPrompt);
-  return new Response(JSON.stringify({ srcUrl }), { status: 200 });
+  return res.status(200).json({ srcUrl });
 }

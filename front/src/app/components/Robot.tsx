38ab7fb2108a491a673029a305d211aa3b1f620b
gadname <gakugaku1114@icloud.com>
2024-02-14 12:05:00 +0900

    database.ymlの更新

--- front/src/app/components/Robot.tsx
+++ front/src/app/components/Robot.tsx
@@ -112,9 +112,9 @@ useEffect(() => {
             {/* アニメーションが表示される */}
           </p>
           <ul>
-          {/* {isTypingSecond && !isTypingThird  && (
+          {isTypingSecond && !isTypingThird  && (
             <HomeTemplate />
-            )} */}
+            )}
             <li>
               <Link href="/three">
               <a></a> {/* next/linkを使用 */}

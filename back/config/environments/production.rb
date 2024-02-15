36e493b8ce112cc93ccd2fe085033283fc55624e
gadname <gakugaku1114@icloud.com>
2024-02-15 16:19:00 +0900

    画像保存先の修正

--- back/config/environments/production.rb
+++ back/config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.hosts << "ai-coten.onrender.com"
-  
+  config.action_controller.asset_host = 'https://ai-coten.onrender.com'
 end

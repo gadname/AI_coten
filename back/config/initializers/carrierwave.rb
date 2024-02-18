dba54a9ce22daf5555183f5280e27bf1c23240c6
gadname <gakugaku1114@icloud.com>
2024-02-18 15:41:11 +0900

    carrierwaveのS3設定

--- back/config/initializers/carrierwave.rb
+++ back/config/initializers/carrierwave.rb
@@ -1,5 +1,10 @@
 CarrierWave.configure do |config|
-    config.asset_host = "https://ai-coten.onrender.com/"
-    config.storage = :file
-    config.cache_storage = :file
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: 'AKIA6N3T5PDTY2H676HC',
+      aws_secret_access_key: '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',
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 's3pf'
+    config.cache_storage = :fog
 end

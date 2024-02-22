421a23f3d496eb4bb0168dcd4a6c56cf3eb8dbb5
gadname <gakugaku1114@icloud.com>
2024-02-22 12:13:20 +0900

    Cloudinaryの設定完了

--- back/config/initializers/carrierwave.rb
+++ back/config/initializers/carrierwave.rb
@@ -1,5 +1,5 @@
 CarrierWave.configure do |config|
-    config.asset_host = "https://ai-coten.onrender.com/"
+    config.asset_host = "https://ai-coten.onrender.com"
     config.storage = :file
     config.cache_storage = :file
 end

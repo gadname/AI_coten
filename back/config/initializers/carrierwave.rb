d53df3def6073349a095241ef5df27ba939c28d1
gadname <gakugaku1114@icloud.com>
2024-02-24 22:39:12 +0900

    変更を元に戻す

--- back/config/initializers/carrierwave.rb
+++ back/config/initializers/carrierwave.rb
@@ -1,13 +1,5 @@
-require 'carrierwave/storage/fog'
-
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/cloudinary'
-  config.fog_directory = nil # Cloudinaryではディレクトリは使用しません
-  config.fog_credentials = {
-    provider: 'Cloudinary',
-    cloud_name: ENV['CLOUDINARY_CLOUD_NAME'],
-    api_key: ENV['CLOUDINARY_API_KEY'],
-    api_secret: ENV['CLOUDINARY_API_SECRET']
-  }
+  config.asset_host = "https://ai-coten.onrender.com"
+  config.storage = :file
+  config.cache_storage = :file
 end

3af59913abc4054a38f151e39fd40e6e1e675528
gadname <gakugaku1114@icloud.com>
2024-02-25 19:36:44 +0900

    S3設定無効

--- back/app/uploaders/image_uploader.rb
+++ back/app/uploaders/image_uploader.rb
@@ -2,11 +2,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-  if Rails.env.production?
-    storage :fog # 本番環境のみ
-  else
-    storage :file # 本番環境以外
-  end
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir

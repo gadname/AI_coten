7d2f7eb531badb9fb997e074937bfbab32d6806d
gadname <gakugaku1114@icloud.com>
2024-03-04 10:50:56 +0900

    Rails側URL修正

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
 allow do
-    origins 'localhost:8000', '127.0.0.1:8000','https://ai-coten.vercel.app'
+    origins 'localhost:8000', '127.0.0.1:8000','https://ai-coten-nu.vercel.app/'
 
     resource "*",
     headers: :any,

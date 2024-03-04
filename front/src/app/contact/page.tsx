03dc1f22ca03623940b93805b1978535ca9d175b
gadname <gakugaku1114@icloud.com>
2024-03-04 16:16:12 +0900

    画像修正２

--- front/src/app/contact/page.tsx
+++ front/src/app/contact/page.tsx
@@ -5,21 +5,6 @@ import Robot from '../components/Robot';
 import type { Metadata } from 'next';
 
 
-export const metadata: Metadata = {
-  metadataBase: new URL('https://ai-coten-nu.vercel.app/'), // 本番環境のアプリ名
-  title: 'Gallery.ai',
-  description: 'Gallery.aiはAI画像生成&デジタル個展サービスです',
-  openGraph: {
-    title: 'Gallery.ai',
-    description: 'Gallery.aiはAI画像生成&デジタル個展サービスです',
-  },
-  twitter: {
-    title: 'Gallery.ai',
-    description: 'Gallery.aiはAI画像生成&デジタル個展サービスです',
-    card: 'summary_large_image',
-  },
-};
-
 function SomeComponent() {
   return (
     <div>

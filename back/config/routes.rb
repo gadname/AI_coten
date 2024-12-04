424491b072eb05655a3448239686be552a25a4b8
gadname <gakugaku1114@icloud.com>
2024-12-04 23:03:31 +0900

    修正2

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -13,9 +13,4 @@ Rails.application.routes.draw do
       end
     end
   end
-
-  # 直接アクセス可能にするためのルート
-  get 'user_images', to: redirect('/api/v1/user_images')
-  get 'user_images/share', to: redirect('/api/v1/user_images/share')
-  post 'user_images/update_urls', to: redirect('/api/v1/user_images/update_urls')
 end
